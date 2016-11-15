d62ba1089476dc0d8c394b441cb2cc247376c225
john navarra <john.l.navarra@gmail.com>
2016-11-14 21:23:28 -0800

    nov 14

---
+++ app/controllers/goods_controller.rb
@@ -0,0 +1,41 @@
+class GoodsController < ApplicationController
+ 
+
+  def home
+    #@good = Good.all
+
+    #<%= @good1.title %>
+    #<%= @good2.title %>
+    #<%= @good3.title%>
+    #      or
+    @goods = Good.all 
+  end  
+
+
+  def index
+    @goods = Good.all 
+  end  
+
+  def show
+    @good = Good.find_by(id: params[:id])
+  end
+
+  def new
+  end
+  
+  def create
+    @good = Good.new({title: params[:title], price: params[:price]})
+    @good.save
+  end  
+
+  def edit
+    @good = Good.find_by(id: params[:id])
+  end
+  
+
+  def update
+    good = Good.find_by(id: params[:id])
+    good.assign_attributes({title: params[:title], price: params[:price]})
+    good.save
+  end  
+end
---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #   "/any_name" to: "controller#method"
+  
+
+  get "/", to:"goods#home"
+
+  get "/home", to: "goods#home" 
+
+  get "/goods", to: "goods#index"
+  
+  get "/goods/new", to: "goods#new"
+  post "/goods", to: "goods#create"
+
+  get "/goods/:id/edit", to: "goods#edit"
+  patch "/goods/:id", to: "goods#update"
+
+  get "/goods/:id", to: "goods#show"
+
+
+
+  
+  #http://localhost:3000/goods/2
+
+end
+
+
+
